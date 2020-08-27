c9a794e8402d1bb923fb339921b49b42f0992640
悟空聊架构 <jackson0585@163.com>
2020-08-27 17:15:24 +0800

    更新 docs/10.并发多线程/04.Java并发必知必会第四弹：线程不安全的集合类.md

--- code/01.Threads/src/com/jackson0714/passjava/threads/ArrayListDemo.java
+++ code/01.Threads/src/com/jackson0714/passjava/threads/ArrayListDemo.java
@@ -1,6 +1,26 @@
 package com.jackson0714.passjava.threads;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * 积木类
+ * @author: 悟空聊架构
+ * @create: 2020-08-27
+ */
+class BuildingBlockWithName {
+    String shape;
+    String name;
+    public BuildingBlockWithName(String shape, String name) {
+        this.shape = shape;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "BuildingBlockWithName{" + "shape='" + shape + ",name=" + name +'}';
+    }
+}
 
 /**
  * @Author: 悟空聊架构| PassJava666
@@ -10,21 +30,86 @@ import java.util.ArrayList;
  */
 public class ArrayListDemo {
     public static void main(String args[]) {
-        demo1();
+        demo2();
     }
 
-
+    /**
+     * Description: 验证单线程下ArrayList线程是安全的
+     * @Author: 公众号 | 悟空聊架构 | PassJava666
+     * @Date: 2020/8/27
+     * @Site: www.jayh.club
+     * @Github: https://github.com/Jackson0714
+     */
     public static void demo1() {
-        ArrayList<BuildingBlock> arrayList = new ArrayList<>();
-        arrayList.add(new BuildingBlock("三角形"));
-        arrayList.add(new BuildingBlock("五角星"));
-        arrayList.add(new BuildingBlock("五边形"));
-        arrayList.add(new BuildingBlock("菱形"));
-        arrayList.add(new BuildingBlock("三角形"));
-        arrayList.add(new BuildingBlock("六角形"));
+        ArrayList<BuildingBlockWithName> arrayList = new ArrayList<>();
+        arrayList.add(new BuildingBlockWithName("三角形", "A"));
+        arrayList.add(new BuildingBlockWithName("四边形", "B"));
+        arrayList.add(new BuildingBlockWithName("五边形", "C"));
+        arrayList.add(new BuildingBlockWithName("六边形", "D"));
+        arrayList.add(new BuildingBlockWithName("五角星", "E"));
 
-        for (BuildingBlock buildingBlock : arrayList) {
-            System.out.println(buildingBlock.toString());
+        for (BuildingBlockWithName buildingBlockWithName : arrayList) {
+            System.out.println(buildingBlockWithName.toString());
+        }
+    }
+
+    /**
+     * Description: 验证多线程下ArrayList线程不安全
+     * @Author: 公众号 | 悟空聊架构 | PassJava666
+     * @Date: 2020/8/27
+     * @Site: www.jayh.club
+     * @Github: https://github.com/Jackson0714
+     */
+    public static void demo2() {
+        ArrayList<BuildingBlockWithName> arrayList = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                Random random = new Random();
+                int type = random.nextInt(4); // 随机生成0/1/2/3/4
+                switch(type) {
+                    case 0: arrayList.add(new BuildingBlockWithName("三角形", "A"));
+                        break;
+                    case 1:  arrayList.add(new BuildingBlockWithName("四边形", "B"));
+                        break;
+                    case 2: arrayList.add(new BuildingBlockWithName("五边形", "C"));
+                        break;
+                    case 3: arrayList.add(new BuildingBlockWithName("六边形", "D"));
+                        break;
+                    case 4: arrayList.add(new BuildingBlockWithName("五角星", "E"));
+                        break;
+                }
+                System.out.println(arrayList);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+    /**
+     * Description: 验证多线程下ArrayList线程不安全
+     * @Author: 公众号 | 悟空聊架构 | PassJava666
+     * @Date: 2020/8/27
+     * @Site: www.jayh.club
+     * @Github: https://github.com/Jackson0714
+     */
+    public static void demo3() {
+        ArrayList<BuildingBlockWithName> arrayList = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                Random random = new Random();
+                int type = random.nextInt(4); // 随机生成0/1/2/3/4
+                switch(type) {
+                    case 0: arrayList.add(new BuildingBlockWithName("三角形", "A"));
+                        break;
+                    case 1:  arrayList.add(new BuildingBlockWithName("四边形", "B"));
+                        break;
+                    case 2: arrayList.add(new BuildingBlockWithName("五边形", "C"));
+                        break;
+                    case 3: arrayList.add(new BuildingBlockWithName("六边形", "D"));
+                        break;
+                    case 4: arrayList.add(new BuildingBlockWithName("五角星", "E"));
+                        break;
+                }
+                System.out.println(arrayList);
+            }, String.valueOf(i)).start();
         }
     }
 }
