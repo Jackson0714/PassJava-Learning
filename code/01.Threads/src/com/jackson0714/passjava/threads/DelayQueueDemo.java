123be4e88b3ee3f69ff86bf39db41de051eee875
悟空聊架构 <jackson0585@163.com>
2020-09-08 18:39:41 +0800

    更新 图解队列

---
+++ code/01.Threads/src/com/jackson0714/passjava/threads/DelayQueueDemo.java
@@ -0,0 +1,16 @@
+package com.jackson0714.passjava.threads;
+
+import java.util.concurrent.DelayQueue;
+
+/**
+ * @Author: 悟空聊架构| PassJava666
+ * @Date: 2020/9/8 18:05
+ * @Site: www.jayh.club
+ * @Github: https://github.com/Jackson0714
+ */
+public class DelayQueueDemo {
+    public static void main(String[] args) {
+
+
+    }
+}
