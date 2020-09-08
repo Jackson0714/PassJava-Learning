49d4563fc9f7858d24bf90be91a5bb88478d9297
悟空聊架构 <jackson0585@163.com>
2020-09-08 17:50:10 +0800

    更新 图解队列

---
+++ code/01.Threads/src/com/jackson0714/passjava/threads/LinkedTransferQueueDemo.java
@@ -0,0 +1,84 @@
+package com.jackson0714.passjava.threads;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: 悟空聊架构 | PassJava666
+ * @Date: 2020/9/8
+ * @Site: www.jayh.club
+ * @Github: https://github.com/Jackson0714
+ */
+public class LinkedTransferQueueDemo {
+    private static LinkedTransferQueue<String> linkedTransferQueue = new LinkedTransferQueue<>();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t transfer A ");
+                linkedTransferQueue.transfer("A");
+
+                System.out.println(Thread.currentThread().getName() + "\t transfer B ");
+                linkedTransferQueue.transfer("B");
+
+                System.out.println(Thread.currentThread().getName() + "\t transfer C ");
+                linkedTransferQueue.transfer("C");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "生产者1").start();
+
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t transfer D ");
+                linkedTransferQueue.transfer("D");
+
+                System.out.println(Thread.currentThread().getName() + "\t transfer E ");
+                linkedTransferQueue.transfer("E");
+
+                System.out.println(Thread.currentThread().getName() + "\t transfer F ");
+                linkedTransferQueue.transfer("F");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "生产者2").start();
+
+        new Thread(() -> {
+            try {
+                String removedElement = "";
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                removedElement = linkedTransferQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take " + removedElement);
+
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                removedElement = linkedTransferQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take " + removedElement);
+
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                removedElement = linkedTransferQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take " + removedElement);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "消费者").start();
+
+
+    }
+}
---
+++ code/01.Threads/src/com/jackson0714/passjava/threads/SynchronousQueueDemo.java
@@ -0,0 +1,65 @@
+package com.jackson0714.passjava.threads;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: 悟空聊架构 | PassJava666
+ * @Date: 2020/9/8
+ * @Site: www.jayh.club
+ * @Github: https://github.com/Jackson0714
+ */
+public class SynchronousQueueDemo {
+    private static BlockingQueue<String> synchronousQueue = new SynchronousQueue<>();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t put A ");
+                synchronousQueue.put("A");
+
+                System.out.println(Thread.currentThread().getName() + "\t put B ");
+                synchronousQueue.put("B");
+
+                System.out.println(Thread.currentThread().getName() + "\t put C ");
+                synchronousQueue.put("C");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            try {
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronousQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take A ");
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronousQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take B ");
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronousQueue.take();
+                System.out.println(Thread.currentThread().getName() + "\t take C ");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t2").start();
+    }
+}
